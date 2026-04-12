33e6492d557d68436d4e376fbfd06ecf8ac2dc1b
x201 <nergej.seganov@proton.me>
2026-04-12 11:20:44 +0200

    sync

--- rubies/mongo/container_in_bg.sh
+++ rubies/mongo/container_in_bg.sh
@@ -1,15 +1,18 @@
 # -d option
-docker run --rm \
-  --name mongo-dev \
+docker run --rm  \
+  --name mongod_contenedor \
   -p 27017:27017 \
-  -v $(pwd)/data:/data/db \
   -v $(pwd)/mongod.conf:/etc/mongod.conf \
   -v $(pwd)/add_user:/docker-entrypoint-initdb.d/init.js \
   -e MONGO_INITDB_ROOT_USERNAME=root \
   -e MONGO_INITDB_ROOT_PASSWORD=root \
- mongo:7  mongod --config /etc/mongod.conf
+ mongo:4.4  mongod --config /etc/mongod.conf
 #  mongo:7 bash 
+#  -v $(pwd)/data:/data/db \
 #  -v $(pwd)/log:/data/log \
-
 #docker exec -it mongo-dev mongosh -u root -p root
+#docker logs -f <container_name_or_id>
+
+# for the x201 > :4.4
+#WARNING: MongoDB 5.0+ requires a CPU with AVX support
 
